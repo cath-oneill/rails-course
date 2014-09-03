72628d58685e9fac511b6265938e7df7a54bfca8
Catherine O'Neill <cathzozo@gmail.com>
2014-09-03 15:13:29 -0500

    started comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903161646) do
+ActiveRecord::Schema.define(version: 20140903191329) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.string   "video_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sounds", force: true do |t|
     t.string   "title"
