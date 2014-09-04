c9e4237d68514c2a2742a52381370044ca8f7a03
Catherine O'Neill <cathzozo@gmail.com>
2014-09-04 11:59:34 -0500

    edit comments, foundation gem

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+  def create
+    comment = Comment.new(comment_params)   
+    comment.video_id = params[:video_id]
+    comment.save
+    redirect_to :back 
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content, :video_id)
+  end
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Metube::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.assets.precompile += %w( vendor/modernizr.js )
   # Code is not reloaded between requests.
   config.cache_classes = true
 
